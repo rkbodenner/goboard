f0c24fc324eaec0d330f9c7f0dbdb7ae42ff768c
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-09-06 01:12:22 -0700

    Tests that mock the web service. Packages using Bower.

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ goboard
 Web front end for boardgame setup app
 
 ## Testing
-0. Start [meeple_mover](https://github.com/rkbodenner/meeple_mover) with a test database.
-1. Make sure `App.MEEPLE_MOVER_URL` points to your meeple_mover service.
-2. Uncomment the `<script src="tests/runner.js"></script>` line at the bottom of `index.html`.
-3. Open the root URL with an added `test` parameter, for example, [http://localhost:8000/index.html?test](http://localhost:8000/index.html?test)
+0. Install Bower (http://bower.io).
+1. Install dependencies via Bower: `bower install`
+2. Make sure, in `js\app.js`, that `App.MEEPLE_MOVER_URL` points to http://localhost:8080. (This is where the tests' HTTP mocks go.)
+3. Uncomment the script tags after "to activate the test runner" at the bottom of `index.html`.
+4. Serve the app, for example using the built-in Python web server: `python -m SimpleHTTPServer 8000`
+5. Open the root URL with an added `test` parameter, for example, [http://localhost:8000/index.html?test](http://localhost:8000/index.html?test)
--- tests/tests.js
+++ tests/tests.js
@@ -18,27 +18,116 @@ module("Integration tests", {
   }
 });
 
+// Fixtures
+
+var jsonGame = {
+  id: 1,
+  name: "Tic-Tac-Toe",
+  SetupRules: [
+    {
+    Id: 1,
+    Description: "Draw 3x3 grid",
+    Details: "",
+    Arity: "Once",
+    Dependencies: null
+    },
+    {
+      Id: 2,
+      Description: "Choose X or O",
+      Details: "",
+      Arity: "Each player",
+      Dependencies: null
+    }
+  ],
+  min_players: 2,
+  max_players: 2
+};
+
+var jsonPlayers = [
+  {
+    id: 1,
+    name: "Player One"
+  },
+  {
+    id: 2,
+    name: "Player Two"
+  }
+];
+
+// Tests
+
 test("load games", function() {
+  var json = [jsonGame];
+  $.mockjax({
+    url: 'http://localhost:8080/games',
+    dataType: 'json',
+    responseText: json,
+  });
+
   visit("/games");
 
   andThen(function() {
-    equal(find("div.game").length, 2, "Two games in the DB");
+    equal(find("div.game").length, 1, "One game");
   });
 });
 
 test("load sessions", function() {
+  var json = [
+    {
+      id: 131,
+      Game: jsonGame,
+      Players: jsonPlayers,
+      SetupAssignments: {
+        1: {
+          Rule: {
+            Id: 2,
+            Description: "Choose X or O",
+            Details: "",
+            Arity: "Each player",
+            Dependencies: null
+          },
+          Owner: {
+            id: 1,
+            name: "Player One"
+          },
+          Done: true
+        }
+      },
+      SetupSteps: [],
+    },
+  ];
+
+  $.mockjax({
+    url: 'http://localhost:8080/sessions',
+    dataType: 'json',
+    responseText: json,
+  });
+
+  $.mockjax({
+    url: 'http://localhost:8080/games/1',
+    dataType: 'json',
+    responseText: jsonGame,
+  });
+
   visit("/sessions");
 
   andThen(function() {
     equal(find("a.active").text(), "Sessions");
-    equal(find("div.session").length, 0, "Zero sessions in the DB");
+    equal(find("div.session").length, 1, "One session");
   });
 });
 
 test("load players", function() {
+  var json = jsonPlayers;
+  $.mockjax({
+    url: 'http://localhost:8080/players',
+    dataType: 'json',
+    responseText: json,
+  });
+
   visit("/players");
 
   andThen(function() {
-    equal(find("div.player").length, 2, "Two players in the DB");
+    equal(find("div.player").length, 2, "Two players");
   });
 });
