e431b71befa5e30d3575a3f464e1558b9af78a9d
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-10-11 15:06:19 -0700

    Upgrade QUnit and use bower to fetch it

--- tests/runner.js
+++ tests/runner.js
@@ -6,9 +6,9 @@ if (window.location.search.indexOf("?test") !== -1) {
     '  <div id="ember-testing"></div>' +
     '</div>' +
     '<link rel="stylesheet" href="tests/runner.css">' +
-    '<link rel="stylesheet" href="tests/vendor/qunit-1.12.0.css">' +
+    '<link rel="stylesheet" href="bower_components/qunit/qunit/qunit.css">' +
     '<script src="bower_components/jquery-mockjax/jquery.mockjax.js"></script>' +
-    '<script src="tests/vendor/qunit-1.12.0.js"></script>' +
+    '<script src="bower_components/qunit/qunit/qunit.js"></script>' +
     '<script src="tests/vendor/ember-qunit-main.js"></script>' +
     '<script src="tests/tests.js"></script>'
   )
